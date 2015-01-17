fe9e473e1f749c192567aaea4efe01b1cfec9cb5
Richo Han <richo2192000@gmail.com>
2015-01-17 14:55:21 +0800

    Remove Redis

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from redis import Redis
-import os
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+
 
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return 'Hello World! I am Richo. I have been seen %s times.' % redis.get('hits')
+    return 'Hello World! I am Richo.'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
