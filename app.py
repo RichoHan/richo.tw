37b9b06119d717aea73195efcc699ba9a45e1163
Richo Han <richo2192000@gmail.com>
2015-01-18 16:07:45 +0800

    Basic UI

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World! I am Richo.'
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,64 @@
+$('.content .resumepage').hide();
+$('.content .contactpage').hide();
+
+var currentpage = $('.content .homepage');
+
+$('.home_btn').click(function() {
+
+	if( currentpage.is($('.content .homepage')) )
+		return;	
+
+	currentpage
+		.transition({
+			animation  : 'fade down',
+			duration   : '500ms',
+			onComplete : function() {
+				$('.content .homepage')
+					.transition('fade up', '500ms')
+				;
+				currentpage = $('.content .homepage');
+			}
+		})
+	;
+
+});
+
+$('.resume_btn').click(function() {
+	
+	if( currentpage.is($('.content .resumepage')) )
+		return;
+
+	currentpage
+		.transition({
+			animation  : 'fade down',
+			duration   : '500ms',
+			onComplete : function() {
+				$('.content .resumepage')
+					.transition('fade up', '500ms')
+				;
+				currentpage = $('.content .resumepage');
+			}
+		})
+	;
+
+});
+
+$('.contact_btn').click(function() {
+	
+	if( currentpage.is($('.content .contactpage')) )
+		return;
+
+	currentpage
+		.transition({
+			animation  : 'fade down',
+			duration   : '500ms',
+			onComplete : function() {
+				$('.content .contactpage')
+					.transition('fade up', '500ms')
+				;
+				currentpage = $('.content .contactpage');
+			}
+		})
+	;
+
+});
