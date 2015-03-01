1b14fc0cdf3e109bccd430616efae73377658f49
Richo <richo2192000@gmail.com>
2015-03-01 18:49:48 +0800

    Add map

--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import json
+from flask import Flask, jsonify
 from flask import render_template
 app = Flask(__name__)
 
@@ -6,6 +7,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+
+@app.route('/geodata')
+def readGeoData():
+    json_data = open('static/map/data/test.geojson')
+    data = json.load(json_data)
+    json_data.close()
+    return jsonify(**data)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
